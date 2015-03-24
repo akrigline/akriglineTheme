7550992bc6a683ca5a1fd64a5ff0c8582eed46c9
akrigline <andrew@akrigline.com>
2015-03-23 23:15:37 -0400

    trimmed content

---
+++ content.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package akrigline
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<div class="entry-content">
+	
+		<?php the_content(); ?>
+
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
